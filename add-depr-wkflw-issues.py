05a2cda2da26e4959a8f46421e71b7ec658506dc
Sarina Canelake <sarina@tcril.org>
2022-02-20 23:31:04 -0500

    temp: skeleton of workflow script

--- add-depr-wkflw-issues.py
+++ add-depr-wkflw-issues.py
@@ -26,23 +26,30 @@ from ghelpers import get_github_headers
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
-def main(org, name, color, description, exclude_private=False):
+def main(org, exclude_private=False, interactive=False):
     """
     Script entrypoint
     """
     gh_headers = get_github_headers()
-
+    branch_name = "add-depr-issue"
     count = 1
-    for repo in get_repos(gh_headers, org, exclude_private):
-        LOG.info("\n\n******* CHECKING REPO: {repo} ({count}) ************\n")
-        create_or_update_label(gh_headers, org, repo, name, color, description)
+    for rname, ssh_url, dbranch, has_issues in get_repos(gh_headers, org, exclude_private):
+        LOG.info("\n\n******* CHECKING REPO: {rname} ({count}) ************\n")
+        clone_repo(ssh_url, dbranch)
+        new_branch(branch_name)
+        add_files(has_issues, interactive)
+        commit_and_pr(gh_headers, org, rname)
         count = count + 1
-    LOG.info("Successfully standardised label {name} across {count} repos")
+    LOG.info("Successfully copied workflow to {count} repos")
 
 def get_repos(gh_headers, org, exclude_private):
     """
     Generator
-    Yields each repo'- name in the org
+    Yields a 4-tuple of repo data:
+    - repo name (str)
+    - ssh url (str)
+    - default branch name (str)
+    - has issues (boolean)
     """
     org_url = "https://api.github.com/orgs/{0}/repos".format(org)
     params = {"page": 1}
@@ -51,11 +58,37 @@ def get_repos(gh_headers, org, exclude_private):
     response = requests.get(org_url, headers=gh_headers, params=params).json()
     while len(response) > 0:
         for repo_data in response:
-            assert not repo_data['private']
-            yield repo_data['name']
+            yield (
+                repo_data['name'],
+                repo_data['ssh_url'],
+                repo_data['default_branch'],
+                repo_data['has_issues']
+            )
         params["page"] = params["page"] + 1
         response = requests.get(org_url, headers=gh_headers, params=params).json()
 
+def clone_repo(ssh_url, dbranch):
+    # if not cloned:
+    # clone repo
+
+    # else
+    # git checkout master
+    # git pull
+    pass
+
+
+def new_branch(branch_name):
+    # git checkout -b add-depr-issue; git push -u origin add-depr-issue
+    pass
+
+
+def add_files(has_issues, interactive):
+    pass
+
+
+def commit_and_pr(gh_headers, org, rname):
+    pass
+
 
 if __name__ == "__main__":
-    main()
+    main("openedx", True, False)
