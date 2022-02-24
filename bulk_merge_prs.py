89d51cc08020fb18d23e49636d97b3b03e141bf8
Sarina Canelake <sarina@tcril.org>
2022-02-24 15:59:47 -0500

    feat: Add script to bulk merge prs

---
+++ bulk_merge_prs.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+"""
+Used to close prs you've got in a list in a json file somewhere
+
+Better would have been: just iterate thru all your repos, search for any open
+PRs with the branch name that you've given the bulk prs, and closing that PR. Oh
+well fix that if you do it again. This would look like:
+
+gurl = "https://api.github.com/repos/openedx/{}/pulls".format(pr_json[1])
+getparams = {"head": "openedx:tcril/depr-automation-workflow"} # name of branch
+r = requests.get(gurl, headers=gh_headers, params=getparams).json()
+pr = r[0]['url'].replace("api.", "")
+pr = pr.replace("pulls/", "pull/")
+pr = pr.replace("repos/", "")
+"""
+
+import argparse
+import datetime
+import json
+import logging
+import requests
+import sys
+import time
+
+from ghelpers import get_github_headers
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+LOG = logging.getLogger(__name__)
+
+def main(path_to_file):
+    stamp = str(datetime.datetime.now())[:19]
+    path_to_failures = "/Users/sarinacanelake/openedx/gh-scripting/output/closed_failures_" + stamp + ".json"
+    gh_headers = get_github_headers()
+    prs_to_close = []
+    failures = []
+    params = {
+        "commit_title": "Merge DEPR automation workflow"
+    }
+    with open(path_to_file) as f:
+        prs_to_close = json.load(f)
+        for pr in prs_to_close:
+            LOG.info("\n********* Closing: {}".format(pr))
+            time.sleep(2)
+            org, repo, number = parse_fields(pr)
+            merge_url = "https://api.github.com/repos/{0}/{1}/pulls/{2}/merge".format(org, repo, number)
+            response = requests.put(merge_url, headers=gh_headers, json=params)
+
+            sc = response.status_code
+            if sc == 200:
+                LOG.info(" Merged - Success!\n")
+            else:
+                LOG.info(" Failure - {}\n".format(sc))
+                failures.append((pr, sc, response.json()))
+
+            time.sleep(2)
+
+    LOG.info(" Writing failures to: {}".format(path_to_failures))
+    with open(path_to_failures, 'w') as f:
+        f.write(json.dumps(failures))
+
+
+def parse_fields(pr):
+    """
+    given: https://github.com/<org>/<repo>/pull/<num>
+    """
+    fields = pr.split("/")
+    org = fields[3]
+    repo = fields[4]
+    num = fields[-1]
+    return (org, repo, num)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="closes each pr in a json list of prs"
+    )
+
+    parser.add_argument(
+        "-f", "--file",
+        help="file to read from"
+    )
+
+    args = parser.parse_args()
+    main(
+        args.file
+    )
