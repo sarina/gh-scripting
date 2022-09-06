0e6abb827196c01ee1142bc496e383dd164e74e0
Sarina Canelake <sarina@tcril.org>
2022-09-06 18:07:04 -0400

    refactor: take common GH functions into general file

--- ghelpers.py
+++ ghelpers.py
@@ -5,6 +5,7 @@ github helpers
 
 import logging
 import os
+import subprocess
 import sys
 
 import github as gh_api
@@ -14,7 +15,7 @@ import requests
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
-def get_repos(gh_headers, org, exclude_private):
+def get_repo_names(gh_headers, org, exclude_private):
     """
     Generator
     Yields each repo'- name in the org
@@ -74,3 +75,133 @@ def get_repos(gh_headers, org, exclude_private):
            )
        params["page"] = params["page"] + 1
        response = requests.get(org_url, headers=gh_headers, params=params).json()
+
+
+def git(command, args, cwd):
+    array = ["/opt/homebrew/bin/git", command]
+    array.extend(args)
+    p1 = subprocess.Popen(
+        array,
+        cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    out = p1.communicate()
+    return out
+
+def clone_repo(root_dir, repo_path, ssh_url, default_branch):
+    """
+    If not already cloned into root_dir, clones repo at that location. If
+    cloned, switches to the repo's default_branch and pulls down the latest
+    changes.
+    """
+    path_exists = os.path.exists(repo_path)
+
+    if not path_exists:
+        git("clone", [ssh_url], root_dir)
+
+    else:
+        git("checkout", [default_branch], repo_path)
+        git("pull", [], repo_path)
+
+
+def new_branch(repo_path, branch_name):
+    """
+    Creates and pushes to remote a new branch called branch_name
+    """
+    _, err = git("checkout", ["-b", branch_name], repo_path)
+    branch_error = "fatal: A branch named '{}' already exists.".format(branch_name)
+    if branch_error in str(err):
+        return False
+
+    git("push", ["-u", "origin", branch_name], repo_path)
+    return True
+
+
+def make_commit(repo_path, commit_msg):
+    """
+    Commits every new file & change in the repo, with the given commit_msg
+    """
+    git("add", ["."], repo_path)
+    git(
+        "commit",
+        ["-a", "-m", commit_msg],
+        repo_path
+    )
+    git("push", [], repo_path)
+
+def make_pr(gh_headers, org, rname, branch_name, dbranch, pr_details):
+    """
+    in the given org & repo, create a pr from specified branch into the default
+    branch with the supplied title and/or body.
+
+    pr_details (dict): specify the title and/or body of the pull request using
+    the keys "title" and "body". Optional; can supply an empty dict.
+    """
+    post_url = "https://api.github.com/repos/{0}/{1}/pulls".format(org, rname)
+    params = {
+        "head": branch_name,
+        "base": dbranch
+    }
+    params.update(pr_details)
+    response = requests.post(post_url, headers=gh_headers, json=params)
+    if response.status_code != 201:
+        raise PrCreationError(response.status_code, response.json())
+
+    pr_url = response.json()["html_url"]
+    LOG.info("PR success: {}".format(pr_url))
+    return pr_url
+
+def mkdir(working_dir, dir_name):
+    p1 = subprocess.Popen(
+        ["/bin/mkdir", dir_name],
+        cwd=working_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    _ = p1.communicate()
+
+def cp(working_dir, filepath, dest_path):
+    p1 = subprocess.Popen(
+        ["cp", filepath, dest_path],
+        cwd=working_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    _ = p1.communicate()
+
+
+def get_repo_path(repo, root_dir):
+    if not root_dir.endswith('/'):
+        root_dir = root_dir + '/'
+    return root_dir + repo
+
+
+class RepoError(Exception):
+    pass
+
+
+class PrCreationError(Exception):
+    def __init__(self, status_code, rjson):
+        self.status_code = status_code
+        self.rjson = rjson
+
+    def __str__(self):
+        error_string = "Problem creating pull request."
+        error_string += "\nGot status code: {}".format(self.status_code)
+        error_string += "\nJSON: {}".format(self.rjson)
+        return error_string
+
+def interactive_commit(repo_path):
+    """
+    Runs `git diff` then waits for user input about whether or not to push changes
+    """
+    # don't call the `git` method because we always want this to go to stdout
+    p1 = subprocess.Popen(
+        ["/opt/homebrew/bin/git", "diff"],
+        cwd=repo_path
+    )
+    _ = p1.communicate()
+
+    cmd = input("Push changes? Y/N: ")
+    while cmd.lower() not in ["y", "n"]:
+        cmd = input("Push changes? Y/N: ")
+    if cmd.lower() == 'n':
+        cmd2 = input("Press Q to quit program, other inputs will continue to next repo: ")
+        if cmd2.lower() == "q":
+            raise KeyboardInterrupt
+        raise RepoError
