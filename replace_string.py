b022b774d22f9165824d95ba008ef01e9d69bad0
Sarina Canelake <sarina@tcril.org>
2022-09-26 16:56:26 -0400

    feat: implement majority of replace_strings functions

--- replace_string.py
+++ replace_string.py
@@ -25,6 +25,7 @@ Notes:
 """
 
 import datetime
+import json
 import logging
 import sys
 import time
@@ -34,7 +35,11 @@ from shell_helpers import *
 
 ### TODO ###
 """
-1. Figure out what argument options are needed to: specify 
+1. Figure out what argument options are needed to:
+  - specify all org vs read from repo list
+  - allow arbitrary string pairs
+  - new branch vs existing branch
+  - make PRs or not
 2. Fix up `__main__` to pull in parse-pr-query results
 """
 
@@ -44,7 +49,15 @@ from shell_helpers import *
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
-def main(org, root_dir, old_string, new_string, exclude_private=False, interactive=False):
+def main(
+        org_or_pr_data,  # is either a string `org_name` or a list of pr_data lists
+        string_pairs,  # a list of pairs (old_string, new_string, commit_msg)
+        branch_name,  # if opening a new branch, else None
+        pr_details,  # when opening the pr. dict of {"title": "title text", "body": "body_text"} or None if not creating a PR
+        root_dir,
+        exclude_private=False,
+        interactive=False
+    ):
     """
     Goes through all repos in an org, clones them (or switches to the default
     branch and then pulls latest changes), searches for the specified string, if
@@ -62,81 +75,129 @@ def main(org, root_dir, old_string, new_string, exclude_private=False, interacti
       awaits user confirmation
     """
     gh_headers = get_github_headers()
+
+    summary = {
+        "commits": 0,    # number of commits made (intentionally not making a PR)
+        "pr_success": 0, # number of PRs made
+        "pr_failure": 0, # number of PRs that had a failure to create
+        "skipped": 0     # number of repos we skipped
+    }
+    overall_output = {}
+
+    if type(org_or_pr_data) == str:
+        org = org_or_pr_data
+        loop_iterator = get_repos(gh_headers, org, exclude_private)
+    else:
+        assert(
+            type(org_or_pr_data) == list and type(org_or_pr_data[0]) == list,
+            f" The repo data must be a list of lists, got {org_or_pr_data}"
+        )
+        loop_iterator = org_or_pr_data
+
+    try:
+        for repo_data in loop_iterator:
+            (rname, ssh_url, dbranch, _, count) = repo_data
+            single_output = []
+            LOG.info("\n\n******* CHECKING REPO: {} ({}) ************".format(rname, count))
+            repo_path = get_repo_path(rname, root_dir)
+
+            # clone repo; if exists, checkout the default branch & pull latest
+            clone_repo(root_dir, repo_path, ssh_url, dbranch)
+
+            # # TODO: Figure out how to handle new branches vs existing ones
+            # # Checkout the already-existing branch_name -- maybe a new arg
+            # branch_created = False
+            # if not checkout_branch(repo_path, branch_name):
+            #     # this branch was never created, or already merged, so
+            #     # create it for the new commit
+            #     new_branch(repo_path, branch_name)
+            #     branch_created = True
+
+            # Go thru and s/old string/new string/g in the repo, then make a commit.
+            # If multiple swaps, does one commit for each swap.
+            for (old_string, new_string, commit_msg) in string_pairs:
+                # Search for the string; fail fast if none exist
+                if not found(old_string, repo_path):
+                    LOG.info(" Did not find string {}".format(old_string))
+                    summary["skipped"] += 1
+                    single_output.append(f"Did not find string {old_string}")
+                    continue
+
+                # Swap old string for new string
+                swap_strings(old_string, new_string, repo_path)
+
+                if interactive:
+                    try:
+                        interactive_commit(repo_path)
+                    except RepoError:
+                        # move on to next repo
+                        continue
+
+                make_commit(repo_path, commit_msg)
+                single_output.append(f"CREATED: {commit_msg}\n")
+
+            if pr_details:
+                try:
+                    LOG.info(f" Making a pull request")
+                    pr_url = make_pr(gh_headers, org, rname, branch_name, dbranch, pr_details)
+                    single_output.append(f"  PR: {pr_url}\n")
+                    summary["pr_success"] += 1
+                except PrCreationError as pr_err:
+                    LOG.info(pr_err.__str__())
+                    # info you need to retry
+                    single_output.append(
+                        f"FAIL REPO INFO: {org}, {rname}, {branch_name}, {dbranch}, {pr_details}\n"
+                    )
+                    summary["pr_failure"] += 1
+
+                time.sleep(5)
+            else:
+                LOG.info(f"  committed to branch with no PR")
+                summary["commits"] += 1
+
+            overall_output["rname"] = single_output
+
+    except KeyboardInterrupt:
+        LOG.info(" Received interrupt, cancelling out")
+
+    finally:
+        ts = str(datetime.datetime.now())[:19]
+        filename = f"output/replace_existing_branch_{ts}.json"
+        with open(filename, "w") as f:
+            f.write(json.dumps(overall_output, indent=4))
+        print(f"Output of {count-1} repos written to {filename}")
+        LOG.info(
+            f" Processed {count} repos; PRs successfully made and {summary['pr_failure']} failures occurred when making PRs."
+        )
+        LOG.info(f"  {summary['commits']} commits created on existing branches.")
+        LOG.info(f"  Skipped {summary['skipped']} repos")
+
+
+if __name__ == "__main__":
+    # is either a string `org_name` or a list of pr_data lists
+    org_or_pr_data = "openedx"
+
+    # a list of pairs (old_string, new_string, commit_msg)
+    string_pairs = [
+        ("uses: edx/.github", "uses: openedx/.github", "fix: update path to .github workflows to read from openedx org")
+    ]
+
+    # if opening a new branch or recommitting on existing branch. Put "None" if providing pr_data lists
     branch_name = "tcril/fix-gh-org-url"
-    commit_msg = "fix: update path to .github workflows to read from openedx org"
+
+    # when opening the pr. dict of {"title": "title text", "body": "body_text"}
+    #  or None if not creating a PR
     pr_details = {
         "title": "Fix github url strings in .github workflows",
         "body": "## This PR was autogenerated\n\nThis pr replaces the old GitHub organization, github.com/edx, with the new GitHub organization, github.com/openedx, in .github/workflow files.\n\nLooking for people to provide review.\n\nRef: https://github.com/openedx/tcril-engineering/issues/42"
     }
 
-    count_commits = 0
-    count_prs = 0
-    count_skipped = 0
-
-    ts = str(datetime.datetime.now())[:19]
-    filename = f"output/replace_existing_branch_{ts}.json"
-    with open(filename, "w") as f:
-        for repo_data in get_repos(gh_headers, org, exclude_private):
-            (rname, ssh_url, dbranch, _, count) = repo_data
-            LOG.info("\n\n******* CHECKING REPO: {} ({}) ************".format(rname, count))
-            if rname == "cs_comments_service":
-                LOG.info(" skipping (was test repo)")
-                continue
-            repo_path = get_repo_path(rname, root_dir)
-            # clone repo; if exists, checkout the default branch & pull latest
-            clone_repo(root_dir, repo_path, ssh_url, dbranch)
-
-            # Search for the string; fail fast if none exist
-            if not found(old_string, repo_path):
-                LOG.info("Did not find string {}".format(old_string))
-                count_skipped += 1
-                f.write(f"NO STRING: {rname}\n")
-                continue
-
-            # Checkout the already-existing branch_name
-            branch_created = False
-            if not checkout_branch(repo_path, branch_name):
-                # this branch was never created, or already merged, so
-                # create it for the new commit
-                new_branch(repo_path, branch_name)
-                branch_created = True
-
-            # Swap old string for new string
-            swap_strings(old_string, new_string, repo_path)
-            if interactive:
-                try:
-                    interactive_commit(repo_path)
-                except RepoError:
-                    # move on to next repo
-                    continue
-
-            make_commit(repo_path, commit_msg)
-
-            if branch_created:
-                try:
-                    LOG.info(f" Making a pull request")
-                    pr_url = make_pr(gh_headers, org, rname, branch_name, dbranch, pr_details)
-                    f.write(f"CREATED PR: {pr_url}\n")
-                    count_prs += 1
-                except PrCreationError as pr_err:
-                    LOG.info(pr_err.__str__())
-                    # info you need to retry
-                    f.write(f"FAILED TO MAKE PR: {org}, {rname}, {branch_name}, {dbranch}, {pr_details}\n")
-            else:
-                LOG.info(f" committed to existing branch")
-                f.write(f"CREATED COMMIT: {rname}\n")
-                count_commits += 1
-
-            time.sleep(5)
-
-    LOG.info(
-        f"Processed {count} repos; {count_prs} PRs successfully made and {count_commits} commits created on existing branches"
-    )
-    LOG.info(f"Skipped {count_skipped} repos as string didn't exist")
-
-
-if __name__ == "__main__":
+    # Where to clone new repos & where existing repos exist
     root_dir = "/Users/sarinacanelake/openedx/"
-    old_string = "uses: edx/.github"
-    new_string = "uses: openedx/.github"
-    main("openedx", root_dir, old_string, new_string, exclude_private=False, interactive=False)
+
+
+    main(
+        org_or_pr_data, string_pairs,
+        branch_name, pr_details, root_dir,
+        exclude_private=False, interactive=False
+    )
