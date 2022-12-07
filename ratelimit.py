d33fd9c8fbac806fe9a05e173d4e3a7a43627474
Sarina Canelake <sarina@tcril.org>
2022-12-07 10:30:58 -0500

    feat: tell me my remaining api calls by checking ratelimit

---
+++ ratelimit.py
@@ -0,0 +1,13 @@
+"""
+what limit do i have left
+"""
+import json
+import requests
+
+from github_helpers import get_github_headers
+
+gh_headers = get_github_headers()
+response = requests.get('https://api.github.com/rate_limit', headers=gh_headers)
+
+rjson = response.json()
+print(json.dumps(rjson, indent=3, sort_keys=True))
